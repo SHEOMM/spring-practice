20eed0eee5b2f2e9e11a5f9a167cd27d872a20a5
SHEOMM <esh0821@snu.ac.kr>
2023-06-24 20:56:12 +0900

    test : member repository test
    회원가입, 중복 가입 테스트

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -3,12 +3,27 @@ package hello.hellospring.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
     @Test
     void 회원가입() {
         //given
@@ -24,6 +39,18 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외(){
         //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+
+        Assertions.assertThat(e.getMessage())
+                .isEqualTo("이미 존재하는 회원입니다.");
     }
 
     @Test
